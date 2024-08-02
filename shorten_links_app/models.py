03b45c28afdcffdd1533b09a018a953426d986e6
Adam Wiercioch <adamwiercioch95@gmail.com>
2024-08-02 10:48:35 +0200

    model link with migration

--- shorten_links_app/models.py
+++ shorten_links_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Link(models.Model):
+    original_url = models.URLField()
+    shortened_path = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f'{self.original_url} -> {self.shortened_path}'
